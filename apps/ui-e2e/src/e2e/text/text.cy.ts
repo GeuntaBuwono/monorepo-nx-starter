e25f810054cb1b8b04dde0a16f5185a5e34f95db
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2023-01-10 06:18:03 +0700

    feat: add storybook

---
+++ apps/ui-e2e/src/e2e/text/text.cy.ts
@@ -0,0 +1,7 @@
+describe('ui: Text component', () => {
+  beforeEach(() => cy.visit('/iframe.html?id=text--primary'));
+    
+    it('should render the component', () => {
+      cy.get('h1').should('contain', 'Welcome to Text!');
+    });
+});
