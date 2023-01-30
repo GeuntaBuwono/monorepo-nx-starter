011038c4bc678d8638cb220fa512476be0b8f1ec
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2023-01-31 03:12:48 +0700

    chore: remove mobile-app

--- apps/mobile-app-e2e/environment.js
+++
@@ -1,6 +0,0 @@
-// setup from https://github.com/wix/Detox/blob/master/docs/Guide.Jest.md#e2eenvironmentjs
-const { DetoxCircusEnvironment } = require('detox/runners/jest');
-
-class CustomDetoxEnvironment extends DetoxCircusEnvironment {}
-
-module.exports = CustomDetoxEnvironment;
