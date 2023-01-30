aba1cb50071de28a075af66a4f37ba6fce2ff541
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2023-01-31 02:51:57 +0700

    chore: migrate web to web-app

--- apps/web-app/jest.config.ts
+++ apps/web-app/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'web',
+  displayName: 'web-app',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/web',
+  coverageDirectory: '../../coverage/apps/web-app',
 };
