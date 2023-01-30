61605dd26a4b80c6035e3e5854837105e88114b4
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2023-01-31 04:44:17 +0700

    chore: remove ui

--- apps/ui-e2e/src/e2e/text/text.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('ui: Text component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=text--primary'));
-
-  it('should render the component', () => {
-    cy.get('h1').should('contain', 'Welcome to Text!');
-  });
-});
--- libs/ui/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/button/button';
-export * from './lib/ui';
