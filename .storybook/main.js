379838323d1ee1b5414a9185cfb7e8025309f4d7
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2023-06-04 01:55:37 +0700

    chore: bump nx 15.9.4

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-essentials'],
+  core: { builder: 'webpack5' },
+  addons: ['@nrwl/react/plugins/storybook'],
   // uncomment the property below if you want to apply some webpack config globally
-  // webpackFinal: async (config, { configType }) => {
-  //   // Make whatever fine-grained changes you need that should apply to all storybook configs
+  webpackFinal: async (config, { configType }) => {
+    // Make whatever fine-grained changes you need that should apply to all storybook configs
 
-  //   // Return the altered config
-  //   return config;
-  // },
+    // Return the altered config
+    return config;
+  },
 };
