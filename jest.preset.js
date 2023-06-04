29eecab0f55cff18547d8a13d6a227ec3fb7fca2
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2023-06-04 22:05:50 +0700

    chore: bump nx 16.3.2

--- jest.preset.js
+++ jest.preset.js
@@ -1,7 +1,13 @@
-const nxPreset = require('@nrwl/jest/preset').default;
+const nxPreset = require('@nx/jest/preset').default;
+
+const globalConf = {
+  collectCoverage: true,
+  coverageDirectory: `${process.env.NX_WORKSPACE_ROOT}/coverage/${process.env['NX_TASK_TARGET_PROJECT']}`,
+};
 
 module.exports = {
   ...nxPreset,
+  ...globalConf,
   /* TODO: Update to latest Jest snapshotFormat
    * By default Nx has kept the older style of Jest Snapshot formats
    * to prevent breaking of any existing tests with snapshots.
