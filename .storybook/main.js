5297c1a5000c1387b331862fb78d069d14e36b52
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2023-09-22 01:31:02 +0700

    chore: remove unused addons @nx/next/plugins/storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: [],
-  addons: ['@nx/next/plugins/storybook'],
+  addons: [],
   // uncomment the property below if you want to apply some webpack config globally
   webpackFinal: async (config, { configType }) => {
     // Make whatever fine-grained changes you need that should apply to all storybook configs
