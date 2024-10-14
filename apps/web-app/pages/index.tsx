59f3e117d7fd0096cc87a28b3fe077931486ca74
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2024-10-14 09:57:40 +0700

    chore: docs remove styled-component

--- apps/web-app/pages/index.tsx
+++ apps/web-app/pages/index.tsx
@@ -9,7 +9,6 @@ function Index() {
   /*
    * Replace the elements below with your own.
    *
-   * Note: The corresponding styles are in the ./index.styled-components file.
    */
   return (
     <StyledPage>
